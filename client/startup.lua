3842af26f1d5a9047cc4e84bec69d8c997487f41
Just1689 <Just1689@gmail.com>
2021-01-27 09:58:56 +0200

    startup

--- client/startup.lua
+++ client/startup.lua
@@ -1,28 +1,48 @@
+print("   ")
+local x
+local y
+local z
+local orientation
+
+-- Get ID
+print("> Getting computer ID")
+print(os.getComputerID())
+print("   ")
 
 -- Get GPS
+print("> Getting x, y, z")
 x,y,z = gps.locate()
+print(x .. ", " ..  y .. ", " .. z)
+print("   ")
+sleep(1)
 
 -- Get orientation
+print("> Getting orientation")
 ok = turtle.forward()
 if ok then
     x2,y2,z2 = gps.locate()
-    if x < x2 then
+    if x > x2 then
         print("facing west")
-    else if x < x2 then
+        orientation = "WEST"
+    elseif x < x2 then
         print("facing east")
+        orientation = "EAST"
     end
 
     if z > z2 then
         print("facing north")
+        orientation = "NORTH"
     else if x < x2 then
         print("facing south")
+        orientation = "SOUTH"
     end
-
+    turtle.back()
+else
+    print("could not move forward")
 end
+print("   ")
 
 -- Get capabilities
-
--- Get name
 
 -- Sign-in
 -- .. block for instructions
