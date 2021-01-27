d79ca24579b8da4c2fb637ed16ad33b8463a865d
Just1689 <Just1689@gmail.com>
2021-01-27 10:04:57 +0200

    startup

--- client/startup.lua
+++ client/startup.lua
@@ -1,44 +1,75 @@
-print("   ")
+print("-----------")
 local x
 local y
 local z
 local orientation
 
 -- Get ID
-print("> Getting computer ID")
+print(">> Getting computer ID")
 print(os.getComputerID())
 print("   ")
 
 -- Get GPS
-print("> Getting x, y, z")
+print(">> Getting x, y, z")
 x,y,z = gps.locate()
 print(x .. ", " ..  y .. ", " .. z)
 print("   ")
 sleep(1)
 
 -- Get orientation
-print("> Getting orientation")
-ok = turtle.forward()
-if ok then
-    x2,y2,z2 = gps.locate()
-    if x > x2 then
-        print("facing west")
-        orientation = "WEST"
-    elseif x < x2 then
-        print("facing east")
-        orientation = "EAST"
+print(">> Getting orientation")
+while orientation == "" do
+    ok = turtle.forward()
+    if ok then
+        x2,y2,z2 = gps.locate()
+        if x > x2 then
+            print("facing west")
+            orientation = "WEST"
+        elseif x < x2 then
+            print("facing east")
+            orientation = "EAST"
+        end
+
+        if z > z2 then
+            print("facing north")
+            orientation = "NORTH"
+        elseif x < x2 then
+            print("facing south")
+            orientation = "SOUTH"
+        end
+        turtle.back()
+    else
+        print("could not move forward")
+    end
+    if orientation == "" then
+        ok = turtle.back()
+        if ok then
+            x2,y2,z2 = gps.locate()
+            if x < x2 then
+                print("facing west")
+                orientation = "WEST"
+            elseif x > x2 then
+                print("facing east")
+                orientation = "EAST"
+            end
+
+            if z < z2 then
+                print("facing north")
+                orientation = "NORTH"
+            elseif x > x2 then
+                print("facing south")
+                orientation = "SOUTH"
+            end
+            turtle.forward()
+        else
+            print("could not move back")
+        end
     end
 
-    if z > z2 then
-        print("facing north")
-        orientation = "NORTH"
-    else if x < x2 then
-        print("facing south")
-        orientation = "SOUTH"
+    if orientation == "" then
+        print("sleeping ... then trying again")
+        sleep(2)
     end
-    turtle.back()
-else
-    print("could not move forward")
 end
 print("   ")
 
